57a4ac8b5a61f3f50f65c2ea7f1d5a61a9f70b65
kgc0019 <kgc0019@tigermail.auburn.edu>
2019-09-26 18:52:21 -0500

    Echo Server?

--- ClientSide.java
+++ ClientSide.java
@@ -1,7 +1,7 @@
 import java.net.*;  // for DatagramSocket, DatagramPacket, and InetAddress
 import java.io.*;   // for IOException
 
-public class SendUDP {
+public class ClientSide {
 
   public static void main(String args[]) throws Exception {
 
@@ -13,21 +13,13 @@ public class SendUDP {
       InetAddress destAddr = InetAddress.getByName(args[0]);  // Destination address
       int destPort = Integer.parseInt(args[1]);               // Destination port
       
-      Friend friend = new Friend(1234567890987654L, "Alice Adams",
-				 (short) 777, 90007, true, true, false);
       
       DatagramSocket sock = new DatagramSocket(); // UDP socket for sending
       
+      String messageString = "Test Message From Client";
+      byte[] messageArray = messageString.getBytes();
       
-      // Use the encoding scheme given on the command line (args[2])
-      FriendEncoder encoder = (args.length == 3 ?
-				  new FriendEncoderBin(args[2]) :
-				  new FriendEncoderBin());
-      
-
-      byte[] codedFriend = encoder.encode(friend); // Encode friend
-      
-      DatagramPacket message = new DatagramPacket(codedFriend, codedFriend.length, 
+      DatagramPacket message = new DatagramPacket(messageArray, messageArray.length, 
 						  destAddr, destPort);
       sock.send(message);
       
--- ServerSide.java
+++ ServerSide.java
@@ -1,7 +1,7 @@
 import java.net.*;  // for DatagramSocket and DatagramPacket
 import java.io.*;   // for IOException
 
-public class RecvUDP {
+public class ServerSide {
 
   public static void main(String[] args) throws Exception {
 
@@ -9,23 +9,27 @@ public class RecvUDP {
 	  throw new IllegalArgumentException("Parameter(s): <Port> [<encoding>]");
       
       int port = Integer.parseInt(args[0]);   // Receiving Port
-      
+     	while(true) { 
       DatagramSocket sock = new DatagramSocket(port);  // UDP socket for receiving      
       DatagramPacket packet = new DatagramPacket(new byte[1024],1024);
       sock.receive(packet);
       
       // Receive binary-encoded friend                              
       // FriendDecoder decoder = new FriendDecoderBin();
-      FriendDecoder decoder = (args.length == 2 ?   // Which encoding              
-				  new FriendDecoderBin(args[1]) :
-				  new FriendDecoderBin() );
+      //FriendDecoder decoder = (args.length == 2 ?   // Which encoding              
+	//			  new FriendDecoderBin(args[1]) :
+	//			  new FriendDecoderBin() 
+	byte[] recv = packet.getData();
+	String echoMessage = new String(recv);
 
 
-      Friend receivedFriend = decoder.decode(packet);
+      //Friend receivedFriend = decoder.decode(packet);
 
-      System.out.println("Received Binary-Encoded Friend");
-      System.out.println(receivedFriend);
+      System.out.println("Received Message");
+      System.out.println(echoMessage)
+      //System.out.println(receivedFriend);
       
       sock.close();
+	}
   }
 }
